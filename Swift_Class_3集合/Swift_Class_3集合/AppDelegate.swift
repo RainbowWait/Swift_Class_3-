9ab986b7ac2514de3b960a400bf2e35e7af203f8
郑小燕 <1136200379@qq.com>
2017-08-14 10:47:26 +0800

    4

--- Swift_Class_3集合/Swift_Class_3集合/AppDelegate.swift
+++ Swift_Class_3集合/Swift_Class_3集合/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
        print("--------")
         print("2222222")
+        print("3333")
         return true
     }
 
